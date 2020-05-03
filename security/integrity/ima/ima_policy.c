7b90449ff5d31227f1021f429ae84744ce0b3f56
Roberto Sassu <roberto.sassu@huawei.com>
2020-05-03 14:33:51 +0200

    ima: Add meta_immutable appraisal type
    Currently, IMA supports the appraise_type=imasig option in the policy to
    require signed file content or metadata. This patch introduces the new
    option appraise_type=meta_immutable to require that file metadata is also
    immutable, i.e. it cannot have been produced by the system itself but only
    from a vendor whose signing key is trusted by the kernel. Currently, this
    requirement can be satisfied only by portable signatures.
    
    The main purpose of this option is to ensure a proper label transition
    during binary execution, when the target label depends on the label of the
    binary being executed. Without it, an administrator might obtain a
    different target label by changing the label of the executable.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- security/integrity/ima/ima_policy.c
+++ security/integrity/ima/ima_policy.c
@@ -1136,7 +1136,8 @@ static bool ima_validate_rule(struct ima_rule_entry *entry)
 
 	if (entry->action != APPRAISE &&
 	    entry->flags & (IMA_DIGSIG_REQUIRED | IMA_MODSIG_ALLOWED |
-			    IMA_CHECK_BLACKLIST | IMA_VALIDATE_ALGOS))
+			    IMA_CHECK_BLACKLIST | IMA_VALIDATE_ALGOS |
+			    IMA_META_IMMUTABLE_REQUIRED))
 		return false;
 
 	/*
@@ -1166,7 +1167,8 @@ static bool ima_validate_rule(struct ima_rule_entry *entry)
 				     IMA_UID | IMA_FOWNER | IMA_FSUUID |
 				     IMA_INMASK | IMA_EUID | IMA_PCR |
 				     IMA_FSNAME | IMA_DIGSIG_REQUIRED |
-				     IMA_PERMIT_DIRECTIO | IMA_VALIDATE_ALGOS))
+				     IMA_PERMIT_DIRECTIO | IMA_VALIDATE_ALGOS |
+				     IMA_META_IMMUTABLE_REQUIRED))
 			return false;
 
 		break;
@@ -1178,7 +1180,8 @@ static bool ima_validate_rule(struct ima_rule_entry *entry)
 				     IMA_INMASK | IMA_EUID | IMA_PCR |
 				     IMA_FSNAME | IMA_DIGSIG_REQUIRED |
 				     IMA_PERMIT_DIRECTIO | IMA_MODSIG_ALLOWED |
-				     IMA_CHECK_BLACKLIST | IMA_VALIDATE_ALGOS))
+				     IMA_CHECK_BLACKLIST | IMA_VALIDATE_ALGOS |
+				     IMA_META_IMMUTABLE_REQUIRED))
 			return false;
 
 		break;
@@ -1599,6 +1602,8 @@ static int ima_parse_rule(char *rule, struct ima_rule_entry *entry)
 				 strcmp(args[0].from, "imasig|modsig") == 0)
 				entry->flags |= IMA_DIGSIG_REQUIRED |
 						IMA_MODSIG_ALLOWED;
+			else if (strcmp(args[0].from, "meta_immutable") == 0)
+				entry->flags |= IMA_META_IMMUTABLE_REQUIRED;
 			else
 				result = -EINVAL;
 			break;
@@ -1999,6 +2004,8 @@ int ima_policy_show(struct seq_file *m, void *v)
 	}
 	if (entry->flags & IMA_CHECK_BLACKLIST)
 		seq_puts(m, "appraise_flag=check_blacklist ");
+	if (entry->flags & IMA_META_IMMUTABLE_REQUIRED)
+		seq_puts(m, "appraise_type=meta_immutable ");
 	if (entry->flags & IMA_PERMIT_DIRECTIO)
 		seq_puts(m, "permit_directio ");
 	rcu_read_unlock();
