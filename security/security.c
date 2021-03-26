74ba7844ee61a737c1e712cbdfe6a04d858c4b55
Roberto Sassu <roberto.sassu@huawei.com>
2021-03-26 09:28:31 +0100

    security: Introduce file_post_open hook
    In preparation to move IMA and EVM to the LSM infrastructure, introduce the
    file_post_open hook.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- security/security.c
+++ security/security.c
@@ -1804,11 +1804,15 @@ int security_file_open(struct file *file)
 	return fsnotify_perm(file, MAY_OPEN);
 }
 
+int security_file_post_open(struct file *file, int mask)
+{
+	return call_int_hook(file_post_open, 0, file, mask);
+}
+
 int security_file_truncate(struct file *file)
 {
 	return call_int_hook(file_truncate, 0, file);
 }
-
 int security_task_alloc(struct task_struct *task, unsigned long clone_flags)
 {
 	int rc = lsm_task_alloc(task);
