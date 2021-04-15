a75ba8341331081c0a2f4b4137af02a796e5f274
Roberto Sassu <roberto.sassu@huawei.com>
2021-04-15 16:25:27 +0200

    ima: Align ima_post_path_mknod() definition with LSM infrastructure
    Change ima_post_path_mknod() definition, so that it can be registered as
    implementation of the path_post_mknod hook.
    
    Also, make sure that ima_post_path_mknod() is executed only if
    (mode & S_IFMT) is equal to zero or S_IFREG.
    
    Add this check to take into account the different placement of the
    path_post_mknod hook (to be introduced) in do_mknodat(). Since the new hook
    will be placed after the switch(), the check ensures that
    ima_post_path_mknod() is invoked as originally intended when it is
    registered as implementation of path_post_mknod.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- fs/namei.c
+++ fs/namei.c
@@ -3966,7 +3966,8 @@ retry:
 			error = vfs_create(idmap, path.dentry->d_inode,
 					   dentry, mode, true);
 			if (!error)
-				ima_post_path_mknod(idmap, dentry);
+				ima_post_path_mknod(idmap, &path, dentry,
+						    mode, dev);
 			break;
 		case S_IFCHR: case S_IFBLK:
 			error = vfs_mknod(idmap, path.dentry->d_inode,
