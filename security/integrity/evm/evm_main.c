0407dcf70b565628ad583ee583f722afc2975169
Roberto Sassu <roberto.sassu@huawei.com>
2021-04-20 11:21:53 +0200

    evm: Align evm_inode_init_security() definition with LSM infrastructure
    This patch changes the evm_inode_init_security() definition to align with
    the LSM infrastructure, in preparation for moving IMA and EVM to that
    infrastructure.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- security/integrity/evm/evm_main.c
+++ security/integrity/evm/evm_main.c
@@ -17,6 +17,7 @@
 #include <linux/xattr.h>
 #include <linux/integrity.h>
 #include <linux/evm.h>
+#include <linux/lsm_hooks.h>
 #include <linux/magic.h>
 #include <linux/posix_acl_xattr.h>
 
@@ -706,29 +707,35 @@ void evm_inode_post_setattr(struct dentry *dentry, int ia_valid)
 /*
  * evm_inode_init_security - initializes security.evm HMAC value
  */
-int evm_inode_init_security(struct inode *inode,
-				 const struct xattr *lsm_xattr,
-				 struct xattr *evm_xattr)
+int evm_inode_init_security(struct inode *inode, struct inode *dir,
+			    const struct qstr *qstr,
+			    struct xattr **xattrs, void *fs_data)
 {
 	struct evm_xattr *xattr_data;
+	struct xattr *new_xattr = lsm_find_xattr_slot(*xattrs);
 	int rc;
 
-	if (!(evm_initialized & EVM_INIT_HMAC) ||
-	    !evm_protected_xattr(lsm_xattr->name))
+	if (!xattrs || !(*xattrs)->name)
 		return 0;
+
+	if (!(evm_initialized & EVM_INIT_HMAC) ||
+	    !evm_protected_xattr((*xattrs)->name))
+		return -EOPNOTSUPP;
 
 	xattr_data = kzalloc(sizeof(*xattr_data), GFP_NOFS);
 	if (!xattr_data)
 		return -ENOMEM;
 
 	xattr_data->data.type = EVM_XATTR_HMAC;
-	rc = evm_init_hmac(inode, lsm_xattr, xattr_data->digest);
+	rc = evm_init_hmac(inode, *xattrs, xattr_data->digest);
 	if (rc < 0)
 		goto out;
 
-	evm_xattr->value = xattr_data;
-	evm_xattr->value_len = hash_digest_size[evm_hash_algo] + 1;
-	evm_xattr->name = XATTR_EVM_SUFFIX;
+	new_xattr->name = XATTR_EVM_SUFFIX;
+	new_xattr->value = xattr_data;
+	new_xattr->value_len = hash_digest_size[evm_hash_algo] + 1;
+	*xattrs = new_xattr;
+
 	return 0;
 out:
 	kfree(xattr_data);
