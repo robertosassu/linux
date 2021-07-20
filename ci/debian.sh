1011c5362ee3a8596df5ac6947fba734793354ae
Roberto Sassu <roberto.sassu@huawei.com>
2021-07-20 17:42:42 +0200

    Add Github Actions configuration

---
+++ ci/debian.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
+set -ex
+
+if [ -z "$CC" ]; then
+	echo "missing \$CC!" >&2
+	exit 1
+fi
+
+# debian.*.sh must be run first
+if [ "$ARCH" ]; then
+	ARCH=":$ARCH"
+	unset CC
+else
+	apt update
+fi
+
+apt="apt install -y --no-install-recommends"
+
+$apt \
+	$CC \
+	libssl-dev$ARCH \
+	make \
+	openssl$ARCH \
+	flex \
+	bison \
+	bc \
+	libc6-dev$ARCH
