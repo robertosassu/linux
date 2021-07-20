f4f5f2a635ad05f7466522b455b903f504b2bc8f
Roberto Sassu <roberto.sassu@huawei.com>
2021-07-20 17:42:42 +0200

    Add Github Actions configuration

---
+++ ci/debian.i386.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
+set -ex
+
+dpkg --add-architecture i386
+apt update
+
+apt install -y --no-install-recommends \
+	linux-libc-dev:i386 \
+	gcc-multilib \
+	pkg-config:i386 \
+	libssl-dev:i386 \
+	openssl:i386
+
+ln -s /usr/include/i386-linux-gnu/openssl/opensslconf.h \
+      /usr/include/openssl/opensslconf.h
