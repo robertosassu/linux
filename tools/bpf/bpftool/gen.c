183f5fb76671989a4f8d90b4ea2661dfb01a1cbf
Roberto Sassu <roberto.sassu@huawei.com>
2022-03-10 16:41:04 +0100

    bpf: Add support for auto attaching LSM programs
    Auto attaching LSM eBPF programs is not yet supported in bpftool. Looking
    at bpf_program__attach_lsm(), it looks that the closest supported program
    type is the tracing one.
    
    Thus, handle the LSM program type in the same way. Although it works, there
    are some warning messages produced by the kernel.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- tools/bpf/bpftool/gen.c
+++ tools/bpf/bpftool/gen.c
@@ -548,6 +548,7 @@ static void codegen_attach_detach(struct bpf_object *obj, const char *obj_name)
 			tp_name = strchr(bpf_program__section_name(prog), '/') + 1;
 			printf("\tint fd = skel_raw_tracepoint_open(\"%s\", prog_fd);\n", tp_name);
 			break;
+		case BPF_PROG_TYPE_LSM:
 		case BPF_PROG_TYPE_TRACING:
 			if (bpf_program__expected_attach_type(prog) == BPF_TRACE_ITER)
 				printf("\tint fd = skel_link_create(prog_fd, 0, BPF_TRACE_ITER);\n");
