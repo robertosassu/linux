8806d237bb99e27c9b4b6131e3b57fd2b45d39ea
Roberto Sassu <roberto.sassu@huawei.com>
2022-03-24 14:52:44 +0100

    bpf: Add DIGLIM
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

---
+++ kernel/bpf/preload/diglim/diglim_kern.c
@@ -0,0 +1,246 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2005,2006,2007,2008 IBM Corporation
+ * Copyright (C) 2022 Huawei Technologies Duesseldorf GmbH
+ *
+ * Authors:
+ * Roberto Sassu <roberto.sassu@huawei.com>
+ * Reiner Sailer <sailer@watson.ibm.com>
+ * Serge Hallyn <serue@us.ibm.com>
+ * Kylene Hall <kylene@us.ibm.com>
+ * Mimi Zohar <zohar@us.ibm.com>
+ *
+ * Implement the kernel space side of DIGLIM.
+ */
+
+#include "vmlinux.h"
+#include <errno.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <string.h>
+#include <asm-generic/mman-common.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+#include "common_kern.h"
+#include "log.h"
+
+#define MAX_DIGESTS	1000000
+
+/* From include/linux/mm.h. */
+#define VM_EXEC	0x00000004
+
+/* From include/linux/mm.h. */
+#define FMODE_WRITE	0x2
+
+/* From include/uapi/asm-generic/fcntl.h. */
+#define __O_TMPFILE	020000000
+#define O_EXCL		00000200
+
+/* From include/linux/fs.h. */
+#define SB_KERNMOUNT	(1<<22) /* this is a kern_mount call */
+
+/* From include/uapi/linux/magic.h. */
+#define TMPFS_MAGIC	0x01021994
+
+char _license[] SEC("license") = "GPL";
+int lsm_mode;
+
+struct inode_storage {
+	u8 state;
+	u8 attribs;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_DIGESTS);
+	__uint(key_size, 1 + MAX_DIGEST_SIZE);
+	__uint(value_size, sizeof(u8));
+} digest_items SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_INODE_STORAGE);
+	__uint(map_flags, BPF_F_NO_PREALLOC);
+	__type(key, int);
+	__type(value, struct inode_storage);
+} inode_storage_map SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_RINGBUF);
+	__uint(max_entries, 1 << 12);
+} ringbuf SEC(".maps");
+
+static void log(enum errors error, u8 *digest, struct file *file)
+{
+	struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+	struct dentry *file_dentry = BPF_CORE_READ(file, f_path.dentry);
+	struct log_entry *e;
+
+	e = bpf_ringbuf_reserve(&ringbuf, sizeof(*e), 0);
+	if (!e)
+		return;
+
+	memset(e, 0, sizeof(*e));
+
+	e->error = error;
+	if (digest)
+		memcpy(e->digest, digest, sizeof(e->digest));
+
+	bpf_probe_read_str(e->filename, NAME_MAX,
+			   BPF_CORE_READ(file_dentry, d_name.name));
+	e->magic = file->f_inode->i_sb->s_magic;
+	bpf_core_read_str(e->task_name, sizeof(e->task_name), &task->comm);
+	e->task_pid = bpf_get_current_pid_tgid() >> 32;
+	bpf_ringbuf_submit(e, 0);
+}
+
+static int digest_lookup(struct file *file)
+{
+	u8 digest[1 + MAX_DIGEST_SIZE] = { 0 };
+	struct inode_storage *inode_storage;
+	u8 *inode_attribs;
+	int ret;
+
+	inode_storage = bpf_inode_storage_get(&inode_storage_map, file->f_inode,
+					0, BPF_LOCAL_STORAGE_GET_F_CREATE);
+	if (inode_storage && (inode_storage->state & INODE_STATE_CHECKED))
+		return 0;
+
+#ifndef HAVE_KERNEL_PATCHES
+	ret = bpf_ima_inode_hash(file->f_inode, digest + 1, sizeof(digest) - 1);
+#else
+	ret = bpf_ima_file_hash(file, digest + 1, sizeof(digest) - 1);
+#endif
+	if (ret < 0) {
+		log(CALC_DIGEST_ERR, NULL, file);
+		return 0; //(lsm_mode == MODE_ENFORCING) ? -EPERM : 0;
+	}
+
+	digest[0] = ret;
+
+	inode_attribs = bpf_map_lookup_elem(&digest_items, digest);
+	if (!inode_attribs) {
+		log(UNKNOWN_DIGEST_ERR, digest, file);
+		return 0; //(lsm_mode == MODE_ENFORCING) ? -EPERM : 0;
+	}
+
+	if (inode_storage) {
+		inode_storage->attribs |= *inode_attribs;
+		inode_storage->state |= INODE_STATE_CHECKED;
+	}
+
+	return 0;
+}
+
+static bool mmap_exec_allowed(struct file *file)
+{
+	struct inode_storage *inode_storage;
+	u64 storage_flags = 0;
+
+	if ((file->f_inode->i_sb->s_magic == TMPFS_MAGIC) &&
+	     (file->f_inode->i_sb->s_flags & SB_KERNMOUNT))
+		storage_flags = BPF_LOCAL_STORAGE_GET_F_CREATE;
+
+	inode_storage = bpf_inode_storage_get(&inode_storage_map, file->f_inode,
+					      0, storage_flags);
+	if (!inode_storage)
+		return false;
+
+	if (storage_flags &
+	    !(inode_storage->state & INODE_STATE_OPENED_WRITTEN))
+		inode_storage->state |= INODE_STATE_MMAP_EXEC_ALLOWED;
+
+	if (!(inode_storage->state & INODE_STATE_MMAP_EXEC_ALLOWED))
+		return false;
+
+	inode_storage->state |= INODE_STATE_MMAP_EXEC_DONE;
+	return true;
+}
+
+SEC("lsm.s/bprm_creds_for_exec")
+int BPF_PROG(exec, struct linux_binprm *bprm)
+{
+	return digest_lookup(bprm->file);
+}
+
+SEC("lsm.s/mmap_file")
+int BPF_PROG(mmap_file, struct file *file, unsigned long reqprot,
+	     unsigned long prot, unsigned long flags)
+{
+	if (!file || !(prot & PROT_EXEC))
+		return 0;
+
+	if (mmap_exec_allowed(file))
+		return 0;
+
+	/* From mmap_violation_check() in ima_main.c. */
+	if (file->f_mapping->i_mmap_writable.counter > 0) {
+		log(MMAP_WRITERS_ERR, NULL, file);
+		return (lsm_mode == MODE_ENFORCING) ? -EPERM : 0;
+	}
+
+	return digest_lookup(file);
+}
+
+SEC("lsm/file_mprotect")
+int BPF_PROG(file_mprotect, struct vm_area_struct *vma, unsigned long prot)
+{
+	/* From ima_file_mprotect() in ima_main.c. */
+	if (!vma->vm_file || !(prot & PROT_EXEC) || (vma->vm_flags & VM_EXEC))
+		return 0;
+
+	if (mmap_exec_allowed(vma->vm_file))
+		return 0;
+
+	log(MPROTECT_ERR, NULL, vma->vm_file);
+	return (lsm_mode == MODE_ENFORCING) ? -EPERM : 0;
+}
+
+SEC("lsm.s/file_open")
+int BPF_PROG(file_open, struct file *file)
+{
+	struct inode_storage *inode_storage;
+	u64 storage_flags = 0;
+
+	if (!(file->f_mode & FMODE_WRITE))
+		return 0;
+
+	if ((file->f_flags & __O_TMPFILE) ||
+	    ((file->f_inode->i_sb->s_magic == TMPFS_MAGIC) &&
+	     (file->f_inode->i_sb->s_flags & SB_KERNMOUNT)))
+		storage_flags = BPF_LOCAL_STORAGE_GET_F_CREATE;
+
+	inode_storage = bpf_inode_storage_get(&inode_storage_map, file->f_inode,
+					      0, storage_flags);
+	if (!inode_storage)
+		return 0;
+
+	if (inode_storage->state & INODE_STATE_MMAP_EXEC_DONE) {
+		log(WRITE_MMAPPED_EXEC, NULL, file);
+		return (lsm_mode == MODE_ENFORCING) ? -EPERM : 0;
+	}
+
+	if (inode_storage->state & INODE_STATE_OPENED_WRITTEN) {
+		inode_storage->state &= ~INODE_STATE_MMAP_EXEC_ALLOWED;
+	} else {
+		if (file->f_flags & __O_TMPFILE)
+			inode_storage->state |= INODE_STATE_MMAP_EXEC_ALLOWED;
+	}
+
+	inode_storage->state &= ~INODE_STATE_CHECKED;
+	inode_storage->state |= INODE_STATE_OPENED_WRITTEN;
+	return 0;
+}
+
+#ifdef HAVE_KERNEL_PATCHES
+SEC("lsm.s/kernel_read_file")
+int BPF_PROG(kernel_read_file, struct file *file, enum kernel_read_file_id id,
+	     bool contents)
+{
+	if (!contents)
+		return 0;
+
+	return digest_lookup(file);
+}
+#endif
