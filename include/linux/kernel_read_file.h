8da9a1e397f516f2537593c234dba9a3606af0d7
Roberto Sassu <roberto.sassu@huawei.com>
2022-04-26 16:36:22 +0200

    fs: Introduce READING_DIGLIM_CONF
    Introduce the new file type, read by the kernel, so that the DIGLIM eBPF
    security module can recognize it and don't attempt to verify it (it will
    be done by the preloading module).
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- include/linux/kernel_read_file.h
+++ include/linux/kernel_read_file.h
@@ -14,6 +14,7 @@
 	id(KEXEC_INITRAMFS, kexec-initramfs)	\
 	id(POLICY, security-policy)		\
 	id(X509_CERTIFICATE, x509-certificate)	\
+	id(DIGLIM_CONF, diglim-conf)	\
 	id(MAX_ID, )
 
 #define __fid_enumify(ENUM, dummy) READING_ ## ENUM,
