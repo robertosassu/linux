cc56264208ff07a4d288e1bc20fcb4e03123b3a6
Roberto Sassu <roberto.sassu@huawei.com>
2022-11-08 16:07:44 +0100

    security: Modify inode_setattr hook
    Add the mnt_userns parameter so that the hook is suitable for EVM.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- include/linux/lsm_hook_defs.h
+++ include/linux/lsm_hook_defs.h
@@ -133,7 +133,8 @@ LSM_HOOK(int, 0, inode_readlink, struct dentry *dentry)
 LSM_HOOK(int, 0, inode_follow_link, struct dentry *dentry, struct inode *inode,
 	 bool rcu)
 LSM_HOOK(int, 0, inode_permission, struct inode *inode, int mask)
-LSM_HOOK(int, 0, inode_setattr, struct dentry *dentry, struct iattr *attr)
+LSM_HOOK(int, 0, inode_setattr, struct user_namespace *mnt_userns,
+	 struct dentry *dentry, struct iattr *attr)
 LSM_HOOK(int, 0, inode_getattr, const struct path *path)
 LSM_HOOK(int, 0, inode_setxattr, struct user_namespace *mnt_userns,
 	 struct dentry *dentry, const char *name, const void *value,
