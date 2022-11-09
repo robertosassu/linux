1c962a27fb11a0a23c590ba8530cb69f4998e0a0
Roberto Sassu <roberto.sassu@huawei.com>
2022-11-09 13:34:18 +0100

    evm: Support multiple LSMs providing an xattr
    Currently, evm_inode_init_security() processes a single LSM xattr from
    the array passed by security_inode_init_security(), and calculates the
    HMAC on it and other inode metadata.
    
    Given that initxattrs() callbacks, called by
    security_inode_init_security(), expect that this array is terminated when
    the xattr name is set to NULL, reuse the same assumption to scan all xattrs
    and to calculate the HMAC on all of them.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/integrity/evm/evm_crypto.c
+++ security/integrity/evm/evm_crypto.c
@@ -390,6 +390,7 @@ int evm_init_hmac(struct inode *inode, const struct xattr *lsm_xattr,
 		  char *hmac_val)
 {
 	struct shash_desc *desc;
+	const struct xattr *xattr;
 
 	desc = init_desc(EVM_XATTR_HMAC, HASH_ALGO_SHA1);
 	if (IS_ERR(desc)) {
@@ -397,7 +398,13 @@ int evm_init_hmac(struct inode *inode, const struct xattr *lsm_xattr,
 		return PTR_ERR(desc);
 	}
 
-	crypto_shash_update(desc, lsm_xattr->value, lsm_xattr->value_len);
+	for (xattr = lsm_xattr; xattr->name != NULL; xattr++) {
+		if (!evm_protected_xattr(xattr->name))
+			continue;
+
+		crypto_shash_update(desc, xattr->value, xattr->value_len);
+	}
+
 	hmac_add_misc(desc, inode, EVM_XATTR_HMAC, hmac_val);
 	kfree(desc);
 	return 0;
