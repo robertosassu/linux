d05bd1dc99ec2689a9156b79d55cc9a12b5ac8d8
Roberto Sassu <roberto.sassu@huawei.com>
2022-11-22 11:01:41 +0100

    evm: Align evm_inode_init_security() definition with LSM infrastructure
    Change the evm_inode_init_security() definition to align with the LSM
    infrastructure. Keep the existing behavior of including in the HMAC
    calculation only the first xattr provided by LSMs.
    
    Changing the evm_inode_init_security() definition requires passing only the
    xattr array allocated by security_inode_init_security(), instead of the
    first LSM xattr and the place where the EVM xattr should be filled. In lieu
    of passing the EVM xattr, EVM must position itself after the last filled
    xattr (by checking the xattr name), since only the beginning of the xattr
    array is given.
    
    Finally, make evm_inode_init_security() return value compatible with the
    inode_init_security hook conventions, i.e. return -EOPNOTSUPP if it is not
    setting an xattr.
    
    EVM is a bit tricky, because xattrs is both an input and an output. If it
    was just output, EVM should have returned zero if xattrs is NULL. But,
    since xattrs is also input, EVM is unable to do its calculations, so return
    -EOPNOTSUPP and handle this error in security_inode_init_security().
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/integrity/evm/evm_main.c
+++ security/integrity/evm/evm_main.c
@@ -864,23 +864,29 @@ void evm_inode_post_setattr(struct dentry *dentry, int ia_valid)
 /*
  * evm_inode_init_security - initializes security.evm HMAC value
  */
-int evm_inode_init_security(struct inode *inode,
-				 const struct xattr *lsm_xattr,
-				 struct xattr *evm_xattr)
+int evm_inode_init_security(struct inode *inode, struct inode *dir,
+			    const struct qstr *qstr,
+			    struct xattr *xattrs)
 {
 	struct evm_xattr *xattr_data;
+	struct xattr *xattr, *evm_xattr;
 	int rc;
 
-	if (!(evm_initialized & EVM_INIT_HMAC) ||
-	    !evm_protected_xattr(lsm_xattr->name))
-		return 0;
+	if (!(evm_initialized & EVM_INIT_HMAC) || !xattrs ||
+	    !evm_protected_xattr(xattrs->name))
+		return -EOPNOTSUPP;
+
+	for (xattr = xattrs; xattr->value != NULL; xattr++)
+		;
+
+	evm_xattr = xattr;
 
 	xattr_data = kzalloc(sizeof(*xattr_data), GFP_NOFS);
 	if (!xattr_data)
 		return -ENOMEM;
 
 	xattr_data->data.type = EVM_XATTR_HMAC;
-	rc = evm_init_hmac(inode, lsm_xattr, xattr_data->digest);
+	rc = evm_init_hmac(inode, xattrs, xattr_data->digest);
 	if (rc < 0)
 		goto out;
 
