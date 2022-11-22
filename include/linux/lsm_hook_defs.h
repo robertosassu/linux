95830bdd11e8178515ed0574f05c36f94889b14f
Roberto Sassu <roberto.sassu@huawei.com>
2022-11-22 10:57:35 +0100

    security: Allow all LSMs to provide xattrs for inode_init_security hook
    Currently, security_inode_init_security() supports only one LSM providing
    an xattr and EVM calculating the HMAC on that xattr, plus other inode
    metadata.
    
    Allow all LSMs to provide one or multiple xattrs, by extending the security
    blob reservation mechanism. Introduce the new lbs_xattr field of the
    lsm_blob_sizes structure, so that each LSM can specify how many xattrs it
    needs, and the LSM infrastructure knows how many xattr slots it should
    allocate.
    
    Dynamically allocate the xattrs array to be populated by LSMs with the
    inode_init_security hook, and pass it to the latter instead of the
    name/value/len triple.
    
    Since the LSM infrastructure, at initialization time, updates the number of
    the requested xattrs provided by each LSM with a corresponding offset in
    the security blob (in this case the xattr array), it makes straightforward
    for an LSM to access the right position in the xattr array.
    
    There is still the issue that an LSM might not fill the xattr, even if it
    requests it (legitimate case, for example it might have been loaded but not
    initialized with a policy). Since users of the xattr array (e.g. the
    initxattrs() callbacks) detect the end of the xattr array by checking if
    the xattr name is NULL, not filling an xattr would cause those users to
    stop scanning xattrs prematurely.
    
    Solve that issue by introducing security_check_compact_filled_xattrs(),
    which does a basic check of the xattr array (if the xattr name is filled,
    the xattr value should be too, and viceversa), and compacts the xattr array
    by removing the holes.
    
    An alternative solution would be to let users of the xattr array know the
    number of elements of the xattr array, so that they don't have to check the
    termination. However, this seems more invasive, compared to a simple move
    of few array elements.
    
    security_check_compact_filled_xattrs() also determines how many xattrs in
    the xattr array have been filled. If there is none, skip
    evm_inode_init_security() and initxattrs(). Skipping the former also avoids
    EVM to crash the kernel, as it is expecting a filled xattr.
    
    Finally, adapt both SELinux and Smack to use the new definition of the
    inode_init_security hook, and to correctly fill the designated slots in the
    xattr array. For Smack, reserve space for the other defined xattrs although
    they are not set yet in smack_inode_init_security().
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- include/linux/lsm_hook_defs.h
+++ include/linux/lsm_hook_defs.h
@@ -112,8 +112,7 @@ LSM_HOOK(int, 0, path_notify, const struct path *path, u64 mask,
 LSM_HOOK(int, 0, inode_alloc_security, struct inode *inode)
 LSM_HOOK(void, LSM_RET_VOID, inode_free_security, struct inode *inode)
 LSM_HOOK(int, 0, inode_init_security, struct inode *inode,
-	 struct inode *dir, const struct qstr *qstr, const char **name,
-	 void **value, size_t *len)
+	 struct inode *dir, const struct qstr *qstr, struct xattr *xattrs)
 LSM_HOOK(int, 0, inode_init_security_anon, struct inode *inode,
 	 const struct qstr *name, const struct inode *context_inode)
 LSM_HOOK(int, 0, inode_create, struct inode *dir, struct dentry *dentry,
