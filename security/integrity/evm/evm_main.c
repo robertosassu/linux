3e62a351cc3807e914807a8f7504dd7b80e9bc30
Roberto Sassu <roberto.sassu@huawei.com>
2022-12-19 15:30:46 +0100

    evm: Complete description of evm_inode_setattr()
    Add the description for missing parameters of evm_inode_setattr() to
    avoid the warning arising with W=n compile option.
    
    Fixes: 817b54aa45db ("evm: add evm_inode_setattr to prevent updating an invalid security.evm")
    Fixes: 0e363cf3fa59 ("security: pass down mount idmapping to setattr hook")
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- security/integrity/evm/evm_main.c
+++ security/integrity/evm/evm_main.c
@@ -798,7 +798,9 @@ static int evm_attr_change(struct user_namespace *mnt_userns,
 
 /**
  * evm_inode_setattr - prevent updating an invalid EVM extended attribute
+ * @mnt_userns: user namespace of the mount
  * @dentry: pointer to the affected dentry
+ * @attr: iattr structure containing the new file attributes
  *
  * Permit update of file attributes when files have a valid EVM signature,
  * except in the case of them having an immutable portable signature.
