8e36cbe09ccdea298e3fdc7869c3861d739ff593
Roberto Sassu <roberto.sassu@huawei.com>
2023-03-09 13:38:57 +0100

    ima: Move to LSM infrastructure
    Remove hardcoded IMA function calls (not for appraisal) from the LSM
    infrastructure, the VFS, NFS and the key subsystem.
    
    Make those functions as static (except for ima_file_check() which is
    exported, and ima_post_key_create_or_update(), which is not in ima_main.c),
    and register them as implementation of the respective hooks in the new
    function init_ima_lsm().
    
    Call init_ima_lsm() from integrity_lsm_init() (renamed from
    integrity_iintcache_init()), to make sure that the integrity subsystem is
    ready at the time IMA hooks are registered. The same will be done for EVM,
    by calling init_evm_lsm() just after init_ima_lsm().
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- security/integrity/integrity.h
+++ security/integrity/integrity.h
@@ -191,6 +191,14 @@ extern struct dentry *integrity_dir;
 
 struct modsig;
 
+#ifdef CONFIG_IMA
+void __init init_ima_lsm(void);
+#else
+static inline void __init init_ima_lsm(void)
+{
+}
+#endif
+
 #ifdef CONFIG_INTEGRITY_SIGNATURE
 
 int integrity_digsig_verify(const unsigned int id, const char *sig, int siglen,
