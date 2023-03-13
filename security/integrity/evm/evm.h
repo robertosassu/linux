810da1d9964127ee1e568999313802aab93731ef
Roberto Sassu <roberto.sassu@huawei.com>
2023-03-13 18:18:36 +0100

    evm: Support multiple LSMs providing an xattr
    Currently, evm_inode_init_security() processes a single LSM xattr from
    the array passed by security_inode_init_security(), and calculates the
    HMAC on it and other inode metadata.
    
    Given that initxattrs() callbacks, called by
    security_inode_init_security(), expect that this array is terminated when
    the xattr name is set to NULL, reuse the same assumption to scan all xattrs
    and to calculate the HMAC on all of them.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: Mimi Zohar <zohar@linux.ibm.com>

--- security/integrity/evm/evm.h
+++ security/integrity/evm/evm.h
@@ -46,6 +46,8 @@ struct evm_digest {
 	char digest[IMA_MAX_DIGEST_SIZE];
 } __packed;
 
+int evm_protected_xattr(const char *req_xattr_name);
+
 int evm_init_key(void);
 int evm_update_evmxattr(struct dentry *dentry,
 			const char *req_xattr_name,
