cb01077f73193e66256ac82bec288079e45674c1
Roberto Sassu <roberto.sassu@huawei.com>
2023-10-16 10:06:34 +0200

    ima: Move to LSM infrastructure
    Move hardcoded IMA function calls (not for appraisal) from various places
    in the kernel to the LSM infrastructure, by registering them to the already
    existing 'integrity' LSM.
    
    Prefer this to making 'ima' and thus 'evm' as standalone LSMs, since IMA
    and EVM use shared integrity metadata, currently managed by the 'integrity'
    LSM.
    
    Make moved functions as static (except ima_post_key_create_or_update(),
    which is not in ima_main.c), and register them as implementation of the
    respective hooks in the new function init_ima_lsm(). Conditionally register
    ima_post_path_mknod() if CONFIG_SECURITY_PATH is enabled, otherwise the
    path_post_mknod hook won't be available.
    
    Call init_ima_lsm() from integrity_lsm_init() (renamed from
    integrity_iintcache_init()), to make sure that the integrity subsystem is
    ready at the time IMA hooks are registered. The same will be done for EVM,
    by calling init_evm_lsm() just after init_ima_lsm().
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Acked-by: Chuck Lever <chuck.lever@oracle.com>

--- security/integrity/iint.c
+++ security/integrity/iint.c
@@ -193,19 +193,26 @@ static void iint_init_once(void *foo)
 	memset(iint, 0, sizeof(*iint));
 }
 
-static int __init integrity_iintcache_init(void)
+static int __init integrity_lsm_init(void)
 {
 	iint_cache =
 	    kmem_cache_create("iint_cache", sizeof(struct integrity_iint_cache),
 			      0, SLAB_PANIC, iint_init_once);
+
+	init_ima_lsm();
 	return 0;
 }
-DEFINE_LSM(integrity) = {
+
+const struct lsm_id integrity_lsmid = {
 	.name = "integrity",
-	.init = integrity_iintcache_init,
-	.order = LSM_ORDER_LAST,
+	.id = LSM_ID_IMA,
 };
 
+DEFINE_LSM(integrity) = {
+	.name = "integrity",
+	.init = integrity_lsm_init,
+	.order = LSM_ORDER_LAST,
+};
 
 /*
  * integrity_kernel_read - read data from the file
